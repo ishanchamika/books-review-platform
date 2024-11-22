0a88f4a5ffeb13ff7a03baba0e1323d1fc6604a6
I.C. Senanayake <136557091+ishanchamika@users.noreply.github.com>
2024-11-23 00:57:27 +0530

    Delete backend directory

--- backend/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { dashboard } = require('../controllers/dashBoardController');
-const verifyToken = require('../middleware/verifyToken');
-
-const router = express.Router();
-
-router.get('/', verifyToken, dashboard);
-
-module.exports = router;
